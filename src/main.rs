c075b55cde78a2e20c0b0f18d1fdcac89f7b31fa
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-02-28 11:39:23 -0600

    NOISSUE - assertions

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,13 @@ fn main() {
     let paths = fs::read_dir("databucket").unwrap();
 
     //Check whether there is data
-    //let is_empty = paths?.next().is_none();
+    //let is_empty = paths.next().is_none();
     let is_empty = false;
 
     if is_empty {
         println!("WARNING: No data to analyze. Verify stdin is nonempty and/or data files are in /databucket")
     } else {
+
         //Loop through files in directory:
         for path in paths {
             //Push resulting hashmap onto vector
@@ -26,12 +27,15 @@ fn main() {
             );
         }
 
-        println!("{:?}",global_vec[1]);
-
         //Assert final vector is not empty:
         assert!(!global_vec.is_empty());
 
-        //Assert that final number of hashmaps in vec equals number of files in directory:
-        //assert!(global_vec.len(),)
+        //Explicit re-scan of directory to validate post-run integrity:
+        let integrity_val = fs::read_dir("databucket").unwrap().count();
+        assert!(global_vec.len()==integrity_val, true);
     }
+
+    //merge all hashmaps
+    //https://docs.rs/object-merge/0.1.0-alpha1/object_merge/
+
 }
