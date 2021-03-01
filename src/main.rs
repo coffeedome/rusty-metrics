a55194c4b23a6eb2a0bbaa1b8916ea23c9662f26
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-02-28 23:08:11 -0600

    STABLE -

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     let input = env::args().nth(1);
     //if input is nothing then look in stdin:
     if input.is_none() {
-        println!("Note: You are running with default stdin input option. The program will run indefinitely if no stdin is provided (via '|')")
+        println!("Note: You are running with default stdin input option. The program will run indefinitely if no stdin is provided (via '|')");
         stdin_handler::read_stdin();
-    } else {
-        //otherwise take input as name of directory (e.g. "databucket")
-        files_handler::handle_files(&input.unwrap());
     }
+
+    //Look for all files in databucket directory, including stdin buffer:
+    files_handler::handle_files("databucket");
 }
