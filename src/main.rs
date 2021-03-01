83b8d820e8ab4a8725b8f07bcd66e0f426de6a4f
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-02-28 22:48:28 -0600

    WORKING - stdin workaround

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let input = env::args().nth(1);
     //if input is nothing then look in stdin:
     if input.is_none() {
-        input.unwrap(); //if no stdin this will throw an error
+        println!("Note: You are running with default stdin input option. The program will run indefinitely if no stdin is provided (via '|')")
         stdin_handler::read_stdin();
     } else {
         //otherwise take input as name of directory (e.g. "databucket")
