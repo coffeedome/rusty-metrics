61859943e68071b702ed6d9d2fbfa44d954da7a0
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-02-28 20:20:44 -0600

    NOISSUE - working but stdin still problematic

---
+++ src/files_handler.rs
@@ -0,0 +1,55 @@
+mod readdata;
+
+use std::collections::HashMap;
+use std::fs;
+
+
+pub fn handle_files(datadirectory: &str){
+
+    //Mutable global vector:
+    let mut global_vec = Vec::new();
+
+    //Scan for files in databucket directory:
+    let paths = fs::read_dir(datadirectory).unwrap();
+
+    //Check whether there is data
+    //let is_empty = paths.next().is_none();
+    let is_empty = false;
+
+    if is_empty {
+        println!("WARNING: No data to analyze. Verify stdin is nonempty and/or data files are in /databucket")
+    } else {
+        //Loop through files in directory:
+        for path in paths {
+            //Push resulting hashmap onto vector
+            global_vec.push(
+                //fn- input:file str; output:hashmap
+                readdata::process_data(path.unwrap().path().to_str().unwrap()),
+            );
+        }
+
+        //Assert final vector is not empty:
+        assert!(!global_vec.is_empty());
+
+        //Explicit re-scan of directory to validate post-run integrity:
+        let integrity_val = fs::read_dir(datadirectory).unwrap().count();
+        assert!(global_vec.len() == integrity_val, true);
+    }
+
+    //Generate final mapping
+    let mut final_map = HashMap::new();
+    for mapping in global_vec.iter() {
+        final_map.extend(mapping);
+    }
+
+    //Generate sorted vector:
+    let mut final_map_vec: Vec<(&String, &i32)> = final_map.into_iter().collect();
+    final_map_vec.sort_by(|a, b| b.1.cmp(&a.1));
+
+    //Print top X entries from vector:
+    //println!("{:#?}", &final_map_vec[..100]);
+    for top_x in &final_map_vec[..100] {
+        println!("{:?}", top_x);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,16 @@
-mod readdata;
 mod stdin_handler;
+mod files_handler;
 
-use std::collections::HashMap;
-use std::fs;
+use std::env;
 
 fn main() {
-    //put stdin in file in directory
-    let _ = stdin_handler::read_stdin();
 
-    //Mutable global vector:
-    let mut global_vec = Vec::new();
-
-    //Scan for files in databucket directory:
-    let paths = fs::read_dir("databucket").unwrap();
-
-    //Check whether there is data
-    //let is_empty = paths.next().is_none();
-    let is_empty = false;
-
-    if is_empty {
-        println!("WARNING: No data to analyze. Verify stdin is nonempty and/or data files are in /databucket")
-    } else {
-        //Loop through files in directory:
-        for path in paths {
-            //Push resulting hashmap onto vector
-            global_vec.push(
-                //fn- input:file str; output:hashmap
-                readdata::process_data(path.unwrap().path().to_str().unwrap()),
-            );
-        }
-
-        //Assert final vector is not empty:
-        assert!(!global_vec.is_empty());
-
-        //Explicit re-scan of directory to validate post-run integrity:
-        let integrity_val = fs::read_dir("databucket").unwrap().count();
-        assert!(global_vec.len() == integrity_val, true);
+    //put stdin in file in directory:
+    let input = env::args().nth(1).unwrap();
+    if input == "-" {
+        let _ = stdin_handler::read_stdin();
     }
+    //process all data:
+    files_handler::handle_files("databucket");
 
-    //Generate final mapping
-    let mut final_map = HashMap::new();
-    for mapping in global_vec.iter() {
-        final_map.extend(mapping);
-    }
-
-    //Generate sorted vector:
-    let mut final_map_vec: Vec<(&String, &i32)> = final_map.into_iter().collect();
-    final_map_vec.sort_by(|a, b| b.1.cmp(&a.1));
-    println!("{:?}", final_map_vec);
 }
