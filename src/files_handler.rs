a165519c4f61c57311f572b45c62f196318861cc
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-02-28 22:37:08 -0600

    NOISSUE - final cleanup before release

--- src/files_handler.rs
+++ src/files_handler.rs
@@ -10,29 +10,22 @@ pub fn handle_files(datadirectory: &str) {
     //Scan for files in databucket directory:
     let paths = fs::read_dir(datadirectory).unwrap();
 
-    //Check whether there is data
-    //let is_empty = paths.next().is_none();
-    let is_empty = false;
-
-    if is_empty {
-        println!("WARNING: No data to analyze. Verify stdin is nonempty and/or data files are in /databucket")
-    } else {
-        //Loop through files in directory:
-        for path in paths {
-            //Push resulting hashmap onto vector
-            global_vec.push(
-                //fn- input:file str; output:hashmap
-                readdata::process_data(path.unwrap().path().to_str().unwrap()),
-            );
-        }
-
-        //Assert final vector is not empty:
-        assert!(!global_vec.is_empty());
-
-        //Explicit re-scan of directory to validate post-run integrity:
-        let integrity_val = fs::read_dir(datadirectory).unwrap().count();
-        assert!(global_vec.len() == integrity_val, true);
+    //Loop through files in directory:
+    for path in paths {
+        //Push resulting hashmap onto vector
+        global_vec.push(
+            //fn- input:file str; output:hashmap
+            readdata::process_data(path.unwrap().path().to_str().unwrap()),
+        );
     }
+
+    //Assert final vector is not empty:
+    assert!(!global_vec.is_empty());
+
+    //Explicit re-scan of directory to validate post-run integrity:
+    let integrity_val = fs::read_dir(datadirectory).unwrap().count();
+    assert!(global_vec.len() == integrity_val, true);
+    
 
     //Generate final mapping
     let mut final_map = HashMap::new();
