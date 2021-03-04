b5b1ac84eaf024058051944a647616441b8dcbec
Esteban Escobar <esteban.e.gomez@gmail.com>
2021-03-04 12:30:42 -0600

    NOISSUE - more cleanup

--- src/files_handler/readdata.rs
+++ src/files_handler/readdata.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 use std::fs;
 
 pub fn process_data(input: &str) -> HashMap<String, i32> {
-
     // //PRE-PROCESS:::convert all to lower case:
     let cnt_str = preprocess_data(input);
 
@@ -12,9 +11,10 @@ pub fn process_data(input: &str) -> HashMap<String, i32> {
     let data_ready = preprocess_data_regex(data_no_special_chars, "\n", " ");
 
     //PROCESS:::capture all 3-string sequences:
-    return  capture_word_sequence(data_ready);
+    return capture_word_sequence(data_ready);
 }
 
+//Function all uppers to lower case
 fn preprocess_data(input: &str) -> String {
     let cnt_raw = fs::read_to_string(input)
         .unwrap()
@@ -24,6 +24,7 @@ fn preprocess_data(input: &str) -> String {
     return cnt_raw;
 }
 
+//Function cleans up string data based on regex match
 fn preprocess_data_regex(data: String, regex_string: &str, replacement: &str) -> String {
     let regex_metachar = regex::escape(regex_string);
     let rgx_special_chars = Regex::new(&regex_metachar)
@@ -33,10 +34,10 @@ fn preprocess_data_regex(data: String, regex_string: &str, replacement: &str) ->
     return rgx_special_chars;
 }
 
+//Generate hashmap <sequence, count> //TODO: parametrize num spaces
 fn capture_word_sequence(regex_string: String) -> HashMap<String, i32> {
-    
     let num_space_regex = Regex::new(r"((?:\S+\s){2}\S+)\s").unwrap();
-    
+
     let mut map = HashMap::new();
     for n in num_space_regex.captures_iter(&regex_string) {
         let count = map.entry(n[1].to_string()).or_insert(0);
